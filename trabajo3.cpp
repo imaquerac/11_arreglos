8861a3c5cc5fd46a2179a6cc8775c2418d3ee53c
imaquerac <imaquerac@unjbg.edu.pe>
2024-07-08 05:02:40 -0500

    ultimos dos trabajos

--- trabajo3.cpp
+++ trabajo3.cpp
@@ -1,1 +1,52 @@
-Este es el tercer archivo
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+float calcularPromedio(int *numeros, int n);
+float calcularDesviacionTipica(int *numeros, int n);
+
+int main()
+{
+    int numeros[100];
+    int n;
+    cout<<"Ingrese la cantidad de numeros a ingresar: ";cin>>n;
+    for(int i = 0; i < n; i++)
+    {
+        int num;
+        do
+        {
+            cout<<"Ingrese un numero entero positivo: ";cin>>num;
+            if(num <= 0)
+            {
+                cout<<"El numero ingresado no es positivo. Intente de nuevo.";
+            }
+        } while(num <= 0);
+        numeros[i] = num;
+    }
+    float promedio = calcularPromedio(numeros, n);
+    float desviacionTipica = calcularDesviacionTipica(numeros, n);
+    cout<<"Promedio general de la serie ingresada: "<<promedio;
+    cout<<"Desviacion tipica de la serie ingresada: "<<desviacionTipica;
+    return 0;
+}
+
+float calcularPromedio(int *numeros, int n)
+{
+    float suma = 0;
+    for(int i = 0; i < n; i++)
+    {
+        suma += numeros[i];
+    }
+    return suma / n;
+}
+
+float calcularDesviacionTipica(int *numeros, int n)
+{
+    float promedio = calcularPromedio(numeros, n);
+    float suma = 0;
+    for(int i = 0; i < n; i++)
+    {
+        suma += pow(numeros[i] - promedio, 2);
+    }
+    return sqrt(suma / n);
+}
--- trabajo4.cpp
+++ trabajo4.cpp
@@ -1,1 +1,65 @@
-Este es el cuarto trabajo
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+
+void generarNumerosAleatorios(int *numeros, int n, int min, int max);
+void calcularFrecuencias(int *numeros, int n, int *frecuencias, int rango);
+void imprimirFrecuencias(int *frecuencias, int rango);
+
+int main()
+{
+    int numeros[100];
+    int n;
+    cout<<"Ingrese la cantidad de numeros aleatorios a generar: ";cin>>n;
+    generarNumerosAleatorios(numeros, n, 1, 15);
+
+    cout<<"Numeros generados: ";
+    for(int i = 0; i < n; i++)
+    {
+        cout<<numeros[i]<<" ";
+    }
+    cout<<endl;
+
+    int rango = 15;
+    int frecuencias[100];
+    calcularFrecuencias(numeros, n, frecuencias, rango);
+
+    imprimirFrecuencias(frecuencias, rango);
+
+    return 0;
+}
+
+void generarNumerosAleatorios(int *numeros, int n, int min, int max)
+{
+    srand(time(0));
+    for(int i = 0; i < n; i++)
+    {
+        numeros[i] = min + rand() % (max - min + 1);
+    }
+}
+
+void calcularFrecuencias(int *numeros, int n, int *frecuencias, int rango)
+{
+    for(int i = 0; i < rango; i++)
+    {
+        frecuencias[i] = 0;
+    }
+    for(int i = 0; i < n; i++)
+    {
+        frecuencias[numeros[i] - 1]++;
+    }
+}
+
+void imprimirFrecuencias(int *frecuencias, int rango)
+{
+    cout<<"                 ";
+    for(int i = 0; i < rango; i++)
+    {
+        if (frecuencias[i] != 0)
+        {
+            cout<<(i + 1)<<" "<<frecuencias[i];
+        }
+    }
+}
