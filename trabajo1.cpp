fc42bf2a00b735638469ca7c9d3ddea9e65871e0
imaquerac <imaquerac@unjbg.edu.pe>
2024-07-08 04:03:12 -0500

    cambios2

--- trabajo1.cpp
+++ trabajo1.cpp
@@ -1,11 +1,117 @@
-<<<<<<< HEAD
 #include<iostream>
 using namespace std;
 
 int main(){
-    
+    float ventas[12];
+    for(int i = 0; i < 12; i++){
+        cout<<"Ingrese las ventas del mes de: ";
+        switch(i){
+            case 0:
+            cout<<"Enero: ";
+            break;
+            case 1:
+            cout<<"Febrero: ";
+            break;
+            case 2:
+            cout<<"Marzo: ";
+            break;
+            case 3:
+            cout<<"Abril: ";
+            break;
+            case 4:
+            cout<<"Mayo: ";
+            break;
+            case 5:
+            cout<<"junio: ";
+            break;
+            case 6:
+            cout<<"Julio: ";
+            break;
+            case 7:
+            cout<<"Agosto: ";
+            break;
+            case 8:
+            cout<<"Septiembre: ";
+            break;
+            case 9:
+            cout<<"Octubre: ";
+            break;
+            case 10:
+            cout<<"Noviembre: ";
+            break;
+            case 11:
+            cout<<"Diciembre: ";
+            break;
+            default:
+            break;
+        }
+        cin>>ventas[i];
+    }
+    float promedio = 0;
+    for(int i = 0; i < 12; i++){
+        promedio += ventas[i];
+    }
+    promedio /= 12;
+    cout<<"Valor de la venta mensual promedio: "<<promedio<<endl;
+    cout<<"Porcentaje de los meses que cumplieron los niveles de venta: ";
+    int contador1 = 0;
+    int contador2 = 0;
+    int contador3 = 0;
+    for(int i = 0; i < 12; i++){
+        if(ventas[i] < 0.4 * promedio){
+            contador1++;
+        }else if(ventas[i] >= 0.4 * promedio && ventas[i] < 0.75 * promedio){
+            contador2++;
+        }
+        else{
+            contador3++;
+        }
+    }
+    cout<<"- Minimo (Menos del 40% del promedio anual): "<<contador1 * 100 / 12 <<"%";
+    cout<<"- Regular (Entre el 40% y el 75% del promedio anual): "<<contador2 * 100 / 12 <<"%";
+    cout<<"- Excelente (Mas de 75% del promedio anual): "<<contador3 * 100 / 12 <<"%";
+    for(int i = 0; i < 12; i++){
+        if (ventas[i] >= 0.75 * promedio){
+            switch(i){
+                case 0:
+                cout<<"- Enero (" <<ventas[i] << ")";
+                break;
+                case 1:
+                cout<<"- Febrero (" <<ventas[i] << ")";
+                break;
+                case 2:
+                cout<<"- Marzo (" <<ventas[i] << ")";
+                break;
+                case 3:
+                cout<<"- Abril (" << ventas[i] << ")";
+                break;
+                case 4:
+                cout<<"- Mayo (" << ventas[i] << ")";
+                break;
+                case 5: 
+                cout<<"- Junio (" << ventas[i] << ")";
+                break;
+                case 6:
+                cout<<"- Julio (" << ventas[i] << ")";
+                break;
+                case 7:
+                cout<<"- Agosto (" << ventas[i] << ")";
+                break;
+                case 8:
+                cout<<"- Septiembre (" << ventas[i] << ")";
+                break;
+                case 9:
+                cout<<"- Octubre (" << ventas[i] << ")";
+                break;
+                case 10:
+                cout<<"- Noviembre (" << ventas[i] << ")";
+                break;
+                case 11:
+                cout<<"- Diciembre (" << ventas[i] << ")";
+                break;
+                default:
+                break;
+            }
+        }
+    }
 }
-=======
-Este es el primer trabajo
-Añadir mas contenido--
->>>>>>> 896facdf9cfdccb8fc7a10919f3d722eb9a2acdd
--- trabajo2.cpp
+++ trabajo2.cpp
@@ -1,2 +1,105 @@
-Este es el segundo trabajo
-prueba--
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+
+int main(){
+    int op, n;
+    float A[100], B[100], C[100];
+    cout<<"Ingrese la cantidad de elementos de los vectores: ";cin>>n;
+    do{
+        system("cls");
+        cout<<"MENU VECTORES";
+        cout<<"================";
+        cout<<"1. Llenar Vector A de manera aeratoria";
+        cout<<"2. Llenar Vector B de manera aeratoria";
+        cout<<"3. Realizar C = A + B";
+        cout<<"4. Realizar c = C - B";
+        cout<<"5. Mostrar los vectores A, B o C";
+        cout<<"0. Salir";
+        cout<<"Ingrese una opcion: ";cin>>op;
+        switch(op){
+            case 1:
+            for(int i = 0; i < n; i++){
+                A[i] = rand() % 201 - 100;
+            }
+            cout<<"Vector A llenado de manera aleatoria";
+            cout<<" ";
+            system("pause");
+            break;
+            case 2:
+            for(int i = 0; i < n; i++){
+                B[i] = rand() % 201 - 100;
+            }
+            cout<<"Vector B llenado de manera aleatoria";
+            cout<<" ";
+            system("pause");
+            break;
+            case 3:
+            for(int i = 0; i < n; i++){
+                C[i] = A[i] + B[i];
+            }
+            cout<<"Vector C = A + B";
+            cout<<" ";
+            system("pause");
+            break;
+            case 4:
+            for(int i = 0; i < n; i++){
+                C[i] = A[i] - B[i];
+            }
+            cout<<"Vector C = B - A";
+            cout<<" ";
+            system("pause");
+            break;
+            case 5:
+            int vectorOption;
+            cout<<"Ingrese el vector a mostrar (1: A, 2: B, 3: C): ";cin>>vectorOption;
+            switch(vectorOption){
+                case 1:
+                cout<<"Vector A: ";
+                for(int i = 0; i < n; i++){
+                    cout<<A[i]<<" ";
+                }
+                cout<<endl;
+                cout<<" ";
+                system("pause");
+                break;
+                case 2:
+                cout<<"Vector B: ";
+                for(int i = 0; i < n; i++){
+                    cout<<B[i]<<" ";
+                }
+                cout<<endl;
+                cout<<" ";
+                system("pause");
+                break;
+                case 3:
+                cout<<"Vector C: ";
+                for(int i = 0; i < n; i++){
+                    cout<<C[i]<<" ";
+                }
+                cout<<endl;
+                cout<<" ";
+                system("pause");
+                break;
+                default:
+                cout<<"Opcion no valida";
+                cout<<" ";
+                system("pause");
+                break;
+            }
+            break;
+            case 0:
+            cout<<"saliendo del programa";
+            cout<<" ";
+            system("pause");
+            break;
+            default: 
+            cout<<"Opcion no valida";
+            cout<<" ";
+            system("pause");
+            break;
+            while (op > 0 && op < 6);
+        }
+    }while(op > 0 && op < 6);
+}
